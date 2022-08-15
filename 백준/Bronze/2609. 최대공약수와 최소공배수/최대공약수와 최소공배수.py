873dcf2386e815af73126ad5be8339b97a896f72
SangGwon Lee <111275002+stu0430@users.noreply.github.com>
2022-08-15 21:08:11 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -0,0 +1,7 @@
+a, b = map(int, input().split())
+for i in range(a, 0, -1):
+    if (a%i==0) and (b%i==0):
+        gcm = i
+        break
+lcm = gcm * (a//gcm) * (b//gcm)
+print(gcm, lcm, sep='\n')
