46f329b37399443e668e26516b79649e93fe4743
SangGwon Lee <111275002+stu0430@users.noreply.github.com>
2022-08-18 19:30:25 +0900

    [Silver IV] Title: 스택, Time: 96 ms, Memory: 32452 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,37 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+dq = deque()
+n = int(input())
+
+for i in range(n):
+    p = input().rstrip()
+    
+    if ' ' in p:
+        p, n = p.split()
+        n = int(n)
+        
+    if p == 'push':
+        dq.append(n)
+        
+    elif p == 'pop':
+        if len(dq) == 0:
+            print(-1)
+        else:
+            print(dq.pop())
+            
+    elif p == 'size':
+        print(len(dq))
+        
+    elif p == 'empty':
+        if len(dq) == 0:
+            print(1)
+        else:
+            print(0)
+            
+    elif p == 'top':
+        if len(dq) == 0:
+            print(-1)
+        else:
+            print(dq[-1])
