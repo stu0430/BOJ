4207b1eba2dd2cd30b09cbb01d168b333e5ebedd
SangGwon Lee <111275002+stu0430@users.noreply.github.com>
2022-08-19 17:47:06 +0900

    [Silver IV] Title: 큐, Time: 92 ms, Memory: 32460 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.py
@@ -0,0 +1,43 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+dq = deque()
+n = int(input())
+
+for i in range(n):
+    p = input().rstrip()
+    
+    if ' ' in p:
+        p, n = p.split()
+        n = int(n)
+        
+    if p == 'push':
+        dq.append(n)
+        
+    elif p == 'pop':
+        if len(dq) == 0:
+            print(-1)
+        else:
+            print(dq.popleft())
+            
+    elif p == 'size':
+        print(len(dq))
+        
+    elif p == 'empty':
+        if len(dq) == 0:
+            print(1)
+        else:
+            print(0)
+    
+    elif p == 'front':
+        if len(dq) == 0:
+            print(-1)
+        else:
+            print(dq[0])
+    
+    elif p == 'back':
+        if len(dq) == 0:
+            print(-1)
+        else:
+            print(dq[-1])
