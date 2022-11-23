e1155e3952cd8c7fb0ba42fcd3e40cd1b93375b2
SangGwon Lee <111275002+stu0430@users.noreply.github.com>
2022-11-23 15:54:43 +0900

    [Silver I] Title: RGB거리, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -0,0 +1,14 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+
+array = [list(map(int, input().split())) for i in range(n)]
+
+for i in range(1, n):
+    array[i][0] = min(array[i - 1][1], array[i - 1][2]) + array[i][0]
+    array[i][1] = min(array[i - 1][0], array[i - 1][2]) + array[i][1]
+    array[i][2] = min(array[i - 1][0], array[i - 1][1]) + array[i][2]
+    
+print(min(array[n - 1][0], array[n - 1][1], array[n - 1][2]))
