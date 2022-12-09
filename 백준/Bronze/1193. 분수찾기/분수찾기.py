7c4b1b1ceda1cf339a8e9eede0d8b581ac17791a
SangGwon Lee <111275002+stu0430@users.noreply.github.com>
2022-12-09 18:42:11 +0900

    [Bronze I] Title: 분수찾기, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/1193. 분수찾기/분수찾기.py
@@ -0,0 +1,16 @@
+import sys
+
+input = sys.stdin.readline
+
+x = int(input())
+
+line = 1
+
+while x > line:
+    x -= line
+    line += 1
+
+if line % 2 == 0:
+    print(f'{x}/{line - x + 1}')
+else:
+    print(f'{line - x + 1}/{x}')
