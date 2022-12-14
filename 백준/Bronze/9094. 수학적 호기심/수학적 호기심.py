b1214120c0f33cee1e274b8eacce0c76f29de7ed
SangGwon Lee <111275002+stu0430@users.noreply.github.com>
2022-12-14 18:59:26 +0900

    [Bronze III] Title: 수학적 호기심, Time: 4068 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/9094. 수학적 호기심/수학적 호기심.py
@@ -0,0 +1,17 @@
+import sys
+
+input = sys.stdin.readline
+
+t = int(input())
+
+for i in range(t):
+    n, m = map(int, input().split())
+    
+    count = 0
+    
+    for a in range(1, n):
+        for b in range(a + 1, n):
+            if (a ** 2 + b ** 2 + m) % (a * b) == 0:
+                count += 1
+                
+    print(count)
