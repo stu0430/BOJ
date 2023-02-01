553dc9aaa3d93f1c274a656a0f0a3da7ad083054
SangGwon Lee <111275002+stu0430@users.noreply.github.com>
2023-02-01 19:42:33 +0900

    [Silver I] Title: 최대공약수, Time: 68 ms, Memory: 41020 KB -BaekjoonHub

---
+++ 백준/Silver/1850. 최대공약수/최대공약수.py
@@ -0,0 +1,14 @@
+import sys
+
+input = sys.stdin.readline
+
+def gcd(a, b):  
+    if b == 0:
+        return a
+    
+    else:
+        return gcd(b, a % b)
+
+a, b = map(int, input().split())
+
+print('1' * gcd(a, b))
