772f60b9c49ca1dc906bf79cb6d63b59e3ac6a3f
SangGwon Lee <111275002+stu0430@users.noreply.github.com>
2023-02-17 19:29:33 +0900

    [Bronze IV] Title: 인공지능 시계, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2530. 인공지능 시계/인공지능 시계.py
@@ -0,0 +1,18 @@
+import sys
+
+input = sys.stdin.readline
+
+h, m, s = map(int, input().split())
+
+t = int(input())
+
+s += t
+m += s // 60
+s %= 60
+
+h += m // 60
+m %= 60
+
+h %= 24
+
+print(h, m, s)
