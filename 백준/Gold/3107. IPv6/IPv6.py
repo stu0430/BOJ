66f7843c89c439ba5647bbbf1fd2fa084ff5a5ab
SangGwon Lee <111275002+stu0430@users.noreply.github.com>
2023-07-12 20:29:20 +0900

    [Gold V] Title: IPv6, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/3107. IPv6/IPv6.py
@@ -0,0 +1,20 @@
+import sys
+
+input = sys.stdin.readline
+
+ipv6 = input().rstrip().split(':')
+
+if ipv6[0] == '':
+    ipv6 = ipv6[1:]
+
+if ipv6[-1] == '':
+    ipv6 = ipv6[:-1]
+
+result = ''
+for i in ipv6:
+    if i == '':
+        result += '0000:' * (8 - len(ipv6) + 1)
+    else:
+        result += i.zfill(4) + ':'
+
+print(result[:-1])
