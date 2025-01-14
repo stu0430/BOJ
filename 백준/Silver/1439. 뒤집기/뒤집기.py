1bc3200fa03af056e91fe52331452a47d08cf777
SangGwon Lee <111275002+stu0430@users.noreply.github.com>
2025-01-14 21:22:17 +0900

    [Silver V] Title: 뒤집기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1439. 뒤집기/뒤집기.py
@@ -0,0 +1,13 @@
+import sys
+
+input = sys.stdin.readline
+
+s = input().rstrip()
+
+count = 0 
+
+for i in range(len(s) - 1):
+    if s[i] != s[i + 1]:
+        count += 1
+        
+print((count + 1) // 2)
