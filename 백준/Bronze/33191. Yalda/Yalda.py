7601f21e09c5178365701330c7df82bbb3e664b5
SangGwon Lee <111275002+stu0430@users.noreply.github.com>
2025-02-06 20:08:18 +0900

    [Bronze IV] Title: Yalda, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/33191. Yalda/Yalda.py
@@ -0,0 +1,16 @@
+n = int(input())
+a = int(input())
+b = int(input())
+c = int(input())
+
+if n >= a:
+    print('Watermelon')
+    
+elif n >= b:
+    print('Pomegranates')
+    
+elif n >= c:
+    print('Nuts')
+
+else:
+    print('Nothing')
