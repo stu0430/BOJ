057a8419720ef6315f4c974b0c66a12bb67c718c
SangGwon Lee <111275002+stu0430@users.noreply.github.com>
2025-02-26 22:17:54 +0900

    [Bronze V] Title: 진흥원 세미나, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/30087. 진흥원 세미나/진흥원 세미나.py
@@ -0,0 +1,15 @@
+rooms = {
+    'Algorithm': 204,
+    'DataAnalysis': 207,
+    'ArtificialIntelligence': 302,
+    'CyberSecurity': 'B101',
+    'Network': 303,
+    'Startup': 501,
+    'TestStrategy': 105
+}
+
+n = int(input().strip())
+
+for _ in range(n):
+    seminar = input().strip()
+    print(rooms[seminar])
