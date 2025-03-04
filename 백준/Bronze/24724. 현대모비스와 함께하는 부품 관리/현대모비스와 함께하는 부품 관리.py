fd92c1f45af8f1120d2eef6d3a4cf28a90b3931b
SangGwon Lee <111275002+stu0430@users.noreply.github.com>
2025-03-04 15:40:29 +0900

    [Bronze III] Title: 현대모비스와 함께하는 부품 관리, Time: 108 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/24724. 현대모비스와 함께하는 부품 관리/현대모비스와 함께하는 부품 관리.py
@@ -0,0 +1,19 @@
+import sys
+
+input = sys.stdin.readline
+
+t = int(input())
+
+i = 1
+
+for _ in range(t):
+    n = int(input())
+    a, b = map(int, input().split())
+
+    for _ in range(n):
+        u, v = map(int, input().split())
+        
+    print(f'Material Management {i}')
+    print('Classification ---- End!')
+    
+    i += 1
