f05a7d6e2786ab307c3c9620f44868069123c0f2
SangGwon Lee <111275002+stu0430@users.noreply.github.com>
2025-08-01 06:28:42 +0900

    [Bronze I] Title: 세준세비, Time: 224 ms, Memory: 41612 KB -BaekjoonHub

---
+++ 백준/Bronze/1524. 세준세비/세준세비.py
@@ -0,0 +1,23 @@
+t = int(input())
+
+for _ in range(t):
+    input()
+    
+    n, m = map(int, input().split())
+    sj = sorted(list(map(int, input().split())), reverse=True)
+    sb = sorted(list(map(int, input().split())), reverse=True)
+    
+    while sj and sb :
+        if sj[-1] >= sb[-1] :
+            sb.pop()
+        else :
+            sj.pop()
+    
+    if sj :
+        print('S')
+        
+    elif sb :
+        print('B')
+        
+    else :
+        print('C')
