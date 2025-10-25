922d2f31b57ff7101e47a5c1786d4edc5b3d52b7
SangGwon Lee <111275002+stu0430@users.noreply.github.com>
2025-10-26 00:25:06 +0900

    [Bronze V] Title: 정보보호학부 동아리 소개, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/28691. 정보보호학부 동아리 소개/정보보호학부 동아리 소개.py
@@ -0,0 +1,16 @@
+n = input()
+
+if n == 'M' : 
+    print('MatKor')
+    
+elif n == 'W' : 
+    print('WiCys')
+    
+elif n == 'C' : 
+    print('CyKor')
+    
+elif n == 'A' : 
+    print('AlKor')
+    
+elif n == '$' : 
+    print('$clear')
